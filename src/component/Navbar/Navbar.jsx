0dac16040b33e26b261a4039296b3f46ebfedfa9
ahmed <ahmed.zaakan@gmail.com>
2024-08-28 17:33:40 +0300

    Navbar section 2

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,25 +1,10 @@
 import { useEffect } from 'react';
 import './Navbar.css'
 import Iconimg from '../../assets/img/logoWhite.png'
-
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
     useEffect(() => {
-        const navbarShrink = () => {
-            const navbarCollapsible = document.querySelector('#mainNav');
-            if (!navbarCollapsible) {
-                return;
-            }
-            if (window.scrollY === 0) {
-                navbarCollapsible.classList.remove('navbar-shrink');
-            } else {
-                navbarCollapsible.classList.add('navbar-shrink');
-            }
-        };
-
-        navbarShrink();
-        window.addEventListener('scroll', navbarShrink);
-
         const mainNav = document.querySelector('#mainNav');
         if (mainNav) {
             new window.bootstrap.ScrollSpy(document.body, {
@@ -27,7 +12,6 @@ const Navbar = () => {
                 rootMargin: '0px 0px -40%',
             });
         }
-
         const navbarToggler = document.querySelector('.navbar-toggler');
         const responsiveNavItems = [].slice.call(
             document.querySelectorAll('#navbarResponsive .nav-link')
@@ -39,28 +23,24 @@ const Navbar = () => {
                 }
             });
         });
-
-        return () => {
-            window.removeEventListener('scroll', navbarShrink);
-        };
     }, []);
-
     return (
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top py-2" id="mainNav">
+        <nav className="navbar navbar-expand-sm navbar-light fixed-top py-3" id="mainNav">
             <div className="container px-4 px-lg-5">
-                <a className="navbar-brand" href="#page-top">
-                    <img src={Iconimg} alt="Icon Img" />
+                <a className="navbar-brand py-0" href="#page-top">
+                    أحمد محمد
                 </a>
                 
-                <button className="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <FaBars className="fa-bars" />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="navbar-nav ms-auto my-2 my-lg-0">
-                        <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#services">Services</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#portfolio">Portfolio</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
+                    <ul className="navbar-nav me-auto my-2 my-lg-0">
+                        <li className="nav-item"><a className="nav-link" href="#about">الرئسية</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#services">أحدث الأعمال</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#portfolio">قالب</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#contact">أحدث المقالات</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#contact">المدونة</a></li>
                     </ul>
                 </div>
             </div>
