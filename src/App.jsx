631806d385d3114cf629ee6a8e5cbaa0ab59c0f6
ahmed <ahmed.zaakan@gmail.com>
2024-08-28 20:07:04 +0300

    Hero section 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Navbar from './component/Navbar/Navbar'
+import Hero from './component/أHero/Hero'
 
 const App = () => {
   return (
     <>
     <Navbar />
+    <Hero />
     </>
   )
 }
---
+++ src/component/أHero/Hero.jsx
@@ -0,0 +1,15 @@
+import './Hero.css'
+const Hero = () => {
+  return (
+    <div className="hero">
+        <div className="conrainer">
+            <div className="row">
+            <div className="col-lg-6 col-md-6 col-sm-12">aaaaaaaaaaa</div>
+            <div className="col-lg-6 col-md-6 col-sm-12">dddd</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
