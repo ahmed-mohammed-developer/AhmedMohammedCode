23c46ae072293c0908a99969f13c880000a29490
ahmed <ahmed.zaakan@gmail.com>
2024-08-29 15:01:07 +0300

    Hero section 2

--- src/component/أHero/Hero.jsx
+++ src/component/أHero/Hero.jsx
@@ -1,11 +1,32 @@
 import './Hero.css'
+import Myimage from '../../assets/img/myimage.png'
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+
+
 const Hero = () => {
   return (
     <div className="hero">
-        <div className="conrainer">
-            <div className="row">
-            <div className="col-lg-6 col-md-6 col-sm-12">aaaaaaaaaaa</div>
-            <div className="col-lg-6 col-md-6 col-sm-12">dddd</div>
+        <div className="container">
+            <div className="row heroflex">
+            <div className="col-lg-6 col-md-6 col-sm-12 ">
+              <div className="heroumage">
+                <img src={Myimage} alt="صورتي الشخصية" />
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 col-sm-12">
+              <div className="herodetalis">
+              <p>أهلا وسهلا بك</p>
+              <h1> أنا أحمد محمد</h1>
+              <h2>مطور واجهات المستخدم</h2>
+              <a href="">السيرة الذاتية</a>
+              <a href="">تواصل معي </a>
+              <div>
+              <FaGithub />
+              <FaLinkedinIn />
+              </div>
+              </div>
+            </div>
             </div>
         </div>
     </div>
