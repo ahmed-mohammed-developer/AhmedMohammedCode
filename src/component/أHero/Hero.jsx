ce31defd415cca71fe6b2fdb9199b0c737b5eda0
ahmed <ahmed.zaakan@gmail.com>
2024-08-29 20:04:05 +0300

    Hero section 3

--- src/component/أHero/Hero.jsx
+++ src/component/أHero/Hero.jsx
@@ -9,21 +9,21 @@ const Hero = () => {
     <div className="hero">
         <div className="container">
             <div className="row heroflex">
-            <div className="col-lg-6 col-md-6 col-sm-12 ">
+            <div className="col-lg-6 col-md-6 col-sm-12 col-lg-6 col-md-6 col-sm-12 d-flex justify-content-evenly">
               <div className="heroumage">
                 <img src={Myimage} alt="صورتي الشخصية" />
               </div>
             </div>
-            <div className="col-lg-6 col-md-6 col-sm-12">
-              <div className="herodetalis">
+            <div className="col-lg-6 col-md-6 col-sm-12 d-flex justify-content-evenly">
+              <div className="herodetalis ">
               <p>أهلا وسهلا بك</p>
               <h1> أنا أحمد محمد</h1>
               <h2>مطور واجهات المستخدم</h2>
               <a href="">السيرة الذاتية</a>
-              <a href="">تواصل معي </a>
-              <div>
-              <FaGithub />
-              <FaLinkedinIn />
+              <a className='ahero' href="">تواصل معي </a>
+              <div className='iconhero'>
+              <FaGithub className='linkdinhero' />
+              <FaLinkedinIn  className='linkdinhero'/>
               </div>
               </div>
             </div>
