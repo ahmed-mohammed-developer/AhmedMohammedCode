9395f72006e933a4f0661ccbffc2166ce52af874
ahmed <ahmed.zaakan@gmail.com>
2024-09-01 12:07:07 +0300

    h

--- src/component/أHero/Hero.jsx
+++ src/component/أHero/Hero.jsx
@@ -1,36 +1,79 @@
-import './Hero.css'
-import Myimage from '../../assets/img/myimage.png'
-import { FaGithub } from "react-icons/fa";
-import { FaLinkedinIn } from "react-icons/fa";
+import './Hero.css';
+import Myimage from '../../assets/img/myimage.png';
+import Myimage2 from '../../assets/img/1small.png';
+import data from '../../../public/data.json';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
+var settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 2, // عرض صورتين في نفس الوقت
+  slidesToScroll: 1, // تمرير صورة واحدة في كل مرة
+  adaptiveHeight: true,
+  initialSlide: 0,
+  rows: 1,
+  slidesPerRow: 1,
+  autoplay: true,
+  autoplaySpeed: 8000,
+  rtl: true,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        initialSlide: 2,
+        dots: true,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        autoplay: true,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false
+      }
+    }
+  ]
+};
 
 const Hero = () => {
+  const dataHero = data.hero;
   return (
     <div className="hero">
-        <div className="container">
-            <div className="row heroflex">
-            <div className="col-lg-6 col-md-6 col-sm-12 col-lg-6 col-md-6 col-sm-12 d-flex justify-content-evenly">
-              <div className="heroumage">
-                <img src={Myimage} alt="صورتي الشخصية" />
+      <div className="container">
+        <Slider {...settings}>
+          {dataHero.map((item, index) => (
+            <div className="row" key={index}>
+              <div className="col-lg-12 col-md-12 col-sm-12">
+                <div className="imghero">
+                <img src={item.img} alt="News" />
+                </div>
               </div>
             </div>
-            <div className="col-lg-6 col-md-6 col-sm-12 d-flex justify-content-evenly">
-              <div className="herodetalis ">
-              <p>أهلا وسهلا بك</p>
-              <h1> أنا أحمد محمد</h1>
-              <h2>مطور واجهات المستخدم</h2>
-              <a href="">السيرة الذاتية</a>
-              <a className='ahero' href="">تواصل معي </a>
-              <div className='iconhero'>
-              <FaGithub className='linkdinhero' />
-              <FaLinkedinIn  className='linkdinhero'/>
-              </div>
-              </div>
-            </div>
-            </div>
-        </div>
+          ))}
+        </Slider>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
